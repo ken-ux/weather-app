5e14e7178559d2a9bdb73776f587f6ae91d54205
Kenny Nguyen <sokenns@gmail.com>
2024-01-02 00:33:39 -0800

    Refactor code so DOM contents work with clearWeather

--- src/modules/parseCurrentWeather.js
+++ src/modules/parseCurrentWeather.js
@@ -9,6 +9,9 @@ export async function parseCurrentWeather(location, days) {
     obj[fields[i]] = data.current[fields[i]];
   }
 
+  const weatherCards = document.querySelector("#weather-cards");
+  const div = document.createElement("div");
+  const h2 = document.createElement("h2");
   const tempF = document.createElement("p");
   tempF.classList = "temp shown";
   const tempC = document.createElement("p");
@@ -16,16 +19,18 @@ export async function parseCurrentWeather(location, days) {
   const condition = document.createElement("p");
   const icon = document.createElement("img");
 
+  h2.textContent = "Current";
   tempF.textContent = obj.temp_f + "°F";
   tempC.textContent = obj.temp_c + "°C";
   condition.textContent = obj.condition.text;
   icon.src = obj.condition.icon;
 
-  const currWeather = document.querySelector("#current-weather");
-  currWeather.appendChild(tempF);
-  currWeather.appendChild(tempC);
-  currWeather.appendChild(condition);
-  currWeather.appendChild(icon);
+  weatherCards.appendChild(div);
+  div.appendChild(h2);
+  div.appendChild(tempF);
+  div.appendChild(tempC);
+  div.appendChild(condition);
+  div.appendChild(icon);
 
   return obj;
 }
