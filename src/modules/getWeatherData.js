a7840fe3b0135fbcc60fce134492c4a5074149f9
Kenny Nguyen <sokenns@gmail.com>
2024-01-02 01:32:07 -0800

    Add check for no matching location

--- src/modules/getWeatherData.js
+++ src/modules/getWeatherData.js
@@ -1,4 +1,7 @@
 export async function getWeatherData(location, days) {
+  if (location === "") {
+    return;
+  }
   try {
     const response = await fetch(
       `http://api.weatherapi.com/v1/forecast.json?key=4da9265abd484c518b043749233112&q=${location}&days=${days}`,
@@ -11,6 +14,6 @@ export async function getWeatherData(location, days) {
       return data;
     }
   } catch (err) {
-    console.log(err);
+    alert(err);
   }
 }
--- src/modules/submitLocation.js
+++ src/modules/submitLocation.js
@@ -1,3 +1,4 @@
+import { getWeatherData } from "./getWeatherData";
 import { parseCurrentWeather } from "./parseCurrentWeather";
 import { parseForecastWeather } from "./parseForecastWeather";
 import { clearWeather } from "./clearWeather";
@@ -7,12 +8,13 @@ export async function submitLocation() {
   const formData = new FormData(form);
   const location = formData.get("location");
 
-  const h1 = document.querySelector("h1");
-  h1.textContent = "Weather Forecast: " + location;
-
-  const button = document.querySelector("#switch-temp");
-  button.textContent = "Change to °C";
-  clearWeather();
-  await parseCurrentWeather(location, 3);
-  await parseForecastWeather(location, 3);
+  getWeatherData(location).then(async (result) => {
+    if (result) {
+      const button = document.querySelector("#switch-temp");
+      button.textContent = "Change to °C";
+      clearWeather();
+      await parseCurrentWeather(location, 3);
+      await parseForecastWeather(location, 3);
+    }
+  });
 }
