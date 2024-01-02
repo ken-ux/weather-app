1bc8f75a91a383602ea91767b618f3e6ffadab95
Kenny Nguyen <sokenns@gmail.com>
2024-01-02 00:22:06 -0800

    Create function to switch temperature scales

---
+++ src/modules/switchTemp.js
@@ -0,0 +1,13 @@
+export function switchTemp() {
+  const button = document.querySelector("#switch-temp");
+  if (button.textContent === "Change to C") {
+    button.textContent = "Change to °F";
+  } else {
+    button.textContent = "Change to °C";
+  }
+
+  const hiddenTemps = document.querySelectorAll(".temp.hidden");
+  const shownTemps = document.querySelectorAll(".temp.shown");
+  hiddenTemps.forEach((temp) => (temp.classList = "temp shown"));
+  shownTemps.forEach((temp) => (temp.classList = "temp hidden"));
+}
