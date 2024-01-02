d7a7707fcc634a41d0c3ddb4a37a8bb8c6d94e78
Kenny Nguyen <sokenns@gmail.com>
2024-01-02 02:55:33 -0800

    Request on HTTPS instead of HTTP

--- src/modules/getWeatherData.js
+++ src/modules/getWeatherData.js
@@ -4,7 +4,7 @@ export async function getWeatherData(location, days) {
   }
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=4da9265abd484c518b043749233112&q=${location}&days=${days}`,
+      `https://api.weatherapi.com/v1/forecast.json?key=4da9265abd484c518b043749233112&q=${location}&days=${days}`,
       { mode: "cors" }
     );
     const data = await response.json();
